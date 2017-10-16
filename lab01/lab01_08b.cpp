bb4df95806e67174d39b4fcef830b7c89a1d0f5a
Christos Gogos <chgogos@gmail.com>
2017-10-16 12:06:29 +0300

    found a bug

--- lab01/lab01_08b.cpp
+++ lab01/lab01_08b.cpp
@@ -1,23 +1,23 @@
 #include <cstdio>
 
-void simple_stats(int a[], int N, double *avg, int *min, int *max) {
+void simple_stats(int a[], int N, double &avg, int &min, int &max) {
   int sum;
-  sum = *min = *max = a[0];
+  sum = min = max = a[0];
   for (int i = 1; i < N; i++) {
     sum += a[i];
-    if (*max < a[i])
-      *max = a[i];
-    if (*min > a[i])
-      *min = a[i];
+    if (max < a[i])
+      max = a[i];
+    if (min > a[i])
+      min = a[i];
   }
-  *avg = (double)sum / (double)N;
+  avg = (double)sum / (double)N;
 }
 
 int main(int argc, char **argv) {
   int a[] = {3, 2, 9, 5, 1};
   double avg;
   int min, max;
-  simple_stats(a, 5, &avg, &min, &max);
+  simple_stats(a, 5, avg, min, max);
   printf("Average= %.2f min=%d max=%d\n", avg, min, max);
   return 0;
 }
