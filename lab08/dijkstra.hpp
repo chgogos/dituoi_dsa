fff87549773fbb53a03301e9fe23ebad4e1c3b68
Christos Gogos <chgogos@gmail.com>
2018-01-18 10:04:39 +0200

    labs 7-8

--- lab08/dijkstra.hpp
+++ lab08/dijkstra.hpp
@@ -1,6 +1,7 @@
 #include <climits>
 #include <map>
 #include <set>
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -8,4 +9,3 @@ using namespace std;
 void compute_shortest_paths_to_all_vertices(
     map<string, vector<pair<int, string>>> &graph, string source,
     map<string, int> &shortest_path_distances);
-    
