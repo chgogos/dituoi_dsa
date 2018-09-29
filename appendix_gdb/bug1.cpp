2c43b32f8479b9c4a0721ee9bcd4ee4dd251d1a4
Christos Gogos <chgogos@gmail.com>
2018-09-29 23:24:45 +0300

    gdb

---
+++ appendix_gdb/bug1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[] = {12, 96, 11, 72, 78};
+    int max = a[0];
+    for (int i = 1; i < 5; i++)
+    {
+        if (a[i] < max)
+        {
+            max = a[i];
+        }
+    }
+    cout << "maximum value: " << max << endl;
+}
